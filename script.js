5bf8379b2a422f571b7ab6d678805ee966034baf
Ruslan <axelezgames@gmail.com>
2023-04-27 22:13:26 +0500

    forced: digit clocks

--- script.js
+++ script.js
@@ -1022,3 +1022,88 @@ ladder.up().up().up().up().up().up().up().down().showStep().down().showStep();
 //     0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
 //   ])
 // );
+
+// function removeEveryOther(arr) {
+//   for (let i = 0; i < arr.lengtth; i++) {
+//     if (arr[i] % 2 == 0) {
+//       arr[i].splice(1, 2);
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log((arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// function Clock({ template }) {
+//   let timer;
+
+//   function render() {
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
+
+//     let output = template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
+
+//     console.log(output);
+//   }
+
+//   this.stop = function () {
+//     clearInterval(timer);
+//   };
+
+//   this.start = function () {
+//     render();
+//     timer = setInterval(render, 1000);
+//   };
+// }
+
+// let clock = new Clock({ template: "h:m:s" });
+// clock.start();
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = "0" + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = "0" + secs;
+
+    let output = this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+let clock = new Clock({ template: "h:m:s" });
+clock.start();
