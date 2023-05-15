60c65188b203e62ac7d922bfd28e1f4adf77e7a2
Ruslan <axelezgames@gmail.com>
2023-05-15 21:27:31 +0500

    fixed

--- script.js
+++ script.js
@@ -2025,8 +2025,6 @@ class PriorityQueue {
   }
 }
 
-// Пример использования:
-
 const graph = new Graph();
 
 graph.addVertex("0");
@@ -2034,6 +2032,15 @@ graph.addVertex("1");
 graph.addVertex("2");
 graph.addVertex("3");
 graph.addVertex("4");
+graph.addVertex("5");
+graph.addVertex("6");
+graph.addVertex("7");
+graph.addVertex("8");
+graph.addVertex("9");
+graph.addVertex("10");
+graph.addVertex("11");
+graph.addVertex("12");
+graph.addVertex("13");
 
 graph.addEdge("0", "1", 12);
 graph.addEdge("0", "2", 12);
@@ -2042,7 +2049,7 @@ graph.addEdge("0", "3", 16);
 graph.addEdge("2", "3", 8);
 graph.addEdge("1", "4", 15);
 graph.addEdge("3", "4", 2);
-graph.addEdge("3", "5", 3);
+graph.addEdge("3", "5", 2);
 graph.addEdge("2", "5", 3);
 graph.addEdge("4", "6", 3);
 graph.addEdge("3", "6", 3);
