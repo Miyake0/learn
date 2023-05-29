e480b4c2d6d65132d5c1b672f6ad8bf6edbb7dd9
Ruslan <axelezgames@gmail.com>
2023-05-29 23:42:29 +0500

    parking

--- script.js
+++ script.js
@@ -2578,17 +2578,29 @@ ladder.up().up().up().up().up().up().up().down().showStep().down().showStep();
 //   return result;
 // };
 
-var new21Game = function (n, k, maxPts) {
-  if (k == 0 || n >= k + maxPts) return 1.0;
-  let sum = 1.0;
-  let result = 0.0;
-  let dp = new Array(n + 1);
-  dp[0] = 1.0;
-  for (let i = 1; i <= n; i++) {
-    dp[i] = sum / maxPts;
-    if (i < k) sum += dp[i];
-    else result += dp[i];
-    if (i - maxPts >= 0) sum -= dp[i - maxPts];
-  }
-  return result;
+// var new21Game = function (n, k, maxPts) {
+//   if (k == 0 || n >= k + maxPts) return 1.0;
+//   let sum = 1.0;
+//   let result = 0.0;
+//   let dp = new Array(n + 1);
+//   dp[0] = 1.0;
+//   for (let i = 1; i <= n; i++) {
+//     dp[i] = sum / maxPts;
+//     if (i < k) sum += dp[i];
+//     else result += dp[i];
+//     if (i - maxPts >= 0) sum -= dp[i - maxPts];
+//   }
+//   return result;
+// };
+
+var ParkingSystem = function (big, medium, small) {
+  this.count = [big, medium, small];
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function (carType) {
+  return this.count[carType - 1]-- > 0;
 };
