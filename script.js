df3cac7fab9f7c820f0b0efdb214d7086ce63566
Ruslan <axelezgames@gmail.com>
2023-06-03 23:21:04 +0500

    numOfMinutes

--- script.js
+++ script.js
@@ -2731,39 +2731,53 @@ ladder.up().up().up().up().up().up().up().down().showStep().down().showStep();
 //   return this.avg.get([start, end].join())[0];
 // };
 
-var shortestPathBinaryMatrix = function (grid) {
-  const m = grid.length;
-  const isValid = (i, j) =>
-    i >= 0 && j >= 0 && i < m && j < m && grid[i][j] == 0;
-  const time = new Array(m).fill(null).map(() => new Array(m).fill(-1));
+// var shortestPathBinaryMatrix = function (grid) {
+//   const m = grid.length;
+//   const isValid = (i, j) =>
+//     i >= 0 && j >= 0 && i < m && j < m && grid[i][j] == 0;
+//   const time = new Array(m).fill(null).map(() => new Array(m).fill(-1));
 
-  const bfs = () => {
-    if (grid[0][0] == 1) {
-      return;
+//   const bfs = () => {
+//     if (grid[0][0] == 1) {
+//       return;
+//     }
+//     const Q = [[0, 0]];
+//     time[0][0] = 1;
+//     while (Q.length) {
+//       let [x, y] = Q.shift();
+//       let t = time[x][y];
+
+//       if (x == m - 1 && y == m - 1) return t;
+
+//       for (let i = -1; i <= 1; i++) {
+//         for (let j = -1; j <= 1; j++) {
+//           if (i == 0 && j == 0) continue;
+//           const X = x + i,
+//             Y = y + j;
+//           if (isValid(X, Y)) {
+//             grid[X][Y] = 1;
+//             Q.push([X, Y]);
+//             time[X][Y] = t + 1;
+//           }
+//         }
+//       }
+//     }
+//   };
+
+//   bfs();
+//   return time[m - 1][m - 1];
+// };
+
+var numOfMinutes = function (n, headID, manager, informTime) {
+  manager.forEach((_, index) => timeFromEmployeeToHead(index));
+
+  return Math.max(...informTime);
+
+  function timeFromEmployeeToHead(id) {
+    if (manager[id] !== -1) {
+      informTime[id] += timeFromEmployeeToHead(manager[id]);
+      manager[id] = -1;
     }
-    const Q = [[0, 0]];
-    time[0][0] = 1;
-    while (Q.length) {
-      let [x, y] = Q.shift();
-      let t = time[x][y];
-
-      if (x == m - 1 && y == m - 1) return t;
-
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          if (i == 0 && j == 0) continue;
-          const X = x + i,
-            Y = y + j;
-          if (isValid(X, Y)) {
-            grid[X][Y] = 1;
-            Q.push([X, Y]);
-            time[X][Y] = t + 1;
-          }
-        }
-      }
-    }
-  };
-
-  bfs();
-  return time[m - 1][m - 1];
+    return informTime[id];
+  }
 };
